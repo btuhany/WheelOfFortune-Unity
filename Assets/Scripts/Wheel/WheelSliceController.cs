9e3bcab0df9f8aff796939205efa21d03162eab1
btuhany <114235184+btuhany@users.noreply.github.com>
2023-11-11 20:03:12 +0300

    +prefab changes
    image components sliced to simple aspect ratio

--- Assets/Scripts/Wheel/WheelSliceController.cs
+++ Assets/Scripts/Wheel/WheelSliceController.cs
@@ -15,12 +15,6 @@ namespace WheelOfFortune.Wheel
         [SerializeField] private RectTransform _itemImageRect;
         [SerializeField] private TextMeshProUGUI _itemCountText;
 
-        //For showing the bomb ui image properly.
-        //Bomb ui image has an 512x512 image dimensions.
-        //It isn't compatible with the image dimensions of rewards. 
-        private Vector2 _initialItemImageSizeDelta;
-        private readonly Vector2 _bombPosOffset = new Vector2(0, 5);
-
         public int SliceIndex { get => _sliceIndex; }
         public WheelItem Content { get => _content; }
         public int ContentCount { get => _contentCount; }
@@ -36,27 +30,14 @@ namespace WheelOfFortune.Wheel
             if(_itemImageRect == null)
                 _itemImageRect = _itemImage.GetComponent<RectTransform>();
         }
-        private void Awake()
-        {
-            _initialItemImageSizeDelta = _itemImageRect.sizeDelta;
-        }
         private void UpdateUIElements(WheelItem item)
         {
             _itemImage.sprite = item.SpriteWheel;
 
             if (item.Type == WheelItem.ItemType.Reward)
-            {
                 _itemCountText.text = "x" + item.Count.ToString();
-                _itemImageRect.sizeDelta = _initialItemImageSizeDelta;
-                _itemImageRect.anchoredPosition = Vector2.zero;
-            }
             else
-            {
                 _itemCountText.text = "";
-                _itemImageRect.sizeDelta = new Vector2(
-                    _itemImageRect.rect.width, _itemImageRect.rect.width);
-                _itemImageRect.anchoredPosition = _bombPosOffset;
-            }
         }
         public void TryRandomizeContentCount()
         {
